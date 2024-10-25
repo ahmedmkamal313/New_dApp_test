edc61348af1cff0d8fb08f1e86f4dcf3923af79f
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2024-10-26 00:01:26 +0300

    Add wallet generation functionality and update runApp with ChangeNotifierProvider

--- web3app/lib/main.dart
+++ web3app/lib/main.dart
@@ -1,33 +1,49 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Flutter framework for building UI.
+import 'package:provider/provider.dart'; // Library for state management.
+import 'package:web3pp/provider.dart'; // Custom provider for wallet functionalities.
 
+/// The main function is the entry point of the Flutter application.
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider<WalletProvider>(
+      create: (context) => WalletProvider(), // Creating an instance of WalletProvider.
+      child: const MyApp(), // The root widget of the application.
+    ),
+  );
 }
 
+/// MyApp is a StatelessWidget that serves as the root of your application.
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor for MyApp widget.
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Accessing the WalletProvider instance.
+    final walletProvider = Provider.of<WalletProvider>(context);
     return MaterialApp(
       title: 'Kaboria', // Title of the application.
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), // Setting a color scheme.
+        useMaterial3: true, // Enabling Material 3 design.
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Kaboria'), // Title of the app bar.
+          title: const Text('Kaboria'), // Title of the app bar.
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center, // Centering the column content vertically.
             children: [
               ElevatedButton(
-                onPressed: () async {},
-                child: Text('Create a new Wallet'),
-                ),
+                onPressed: () async {
+                  // Generating a new wallet when the button is pressed.
+                  final mnemonic = walletProvider.generateMnemonic();
+                  final privateKey = await walletProvider.getPrivateKey(mnemonic);
+                  final publickKey = await walletProvider.getPublicKey(privateKey);
+                  // You can add further actions here, like displaying the generated keys.
+                },
+                child: const Text('Create a new Wallet'), // Button label.
+              ),
             ],
           ),
         ),

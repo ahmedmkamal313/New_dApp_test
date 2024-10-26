36f534a19a1f5f261c2fa2307e2e4964a4a4e610
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2024-10-26 04:08:16 +0300

    add Basic UI for wallet onboarding screen

--- web3app/lib/screens/wallet_onboarding_screen.dart
+++ web3app/lib/screens/wallet_onboarding_screen.dart
@@ -1,3 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:web3pp/screens/wallet_generate_screen.dart';
-import 'package:web3pp/screens/wallet_import_screen.dart';
+import 'package:flutter/material.dart'; // Flutter framework for building UI.
+import 'package:web3pp/screens/wallet_generate_screen.dart'; // Screen for creating a new wallet.
+import 'package:web3pp/screens/wallet_import_screen.dart'; // Screen for importing a new wallet screen.
+
+class WalletOptionsScreen extends StatelessWidget {
+  const WalletOptionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: const Text(
+                'Welcome To Kaboria',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            // First SizedBox - Space after the welcome text
+            const SizedBox(height: 24),
+
+            // Logo Container
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: 150,
+                height: 200,
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+
+            // Second SizedBox - Space after the logo
+            const SizedBox(height: 50),
+
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context, MaterialPageRoute(builder: context => const Wallet) )
+              }
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
